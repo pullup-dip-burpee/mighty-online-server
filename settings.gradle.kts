966e99dc3d2c1de71d4a4bed23c0ceb8dc3f395a
pullup-dip-burpee <silverysky@snu.ac.kr>
2022-03-12 02:45:45 +0900

    Init springboot project:

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "mighty-online-server"
---
+++ src/test/kotlin/com/mightyonline/mightyonlineserver/MightyOnlineServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mightyonline.mightyonlineserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MightyOnlineServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
